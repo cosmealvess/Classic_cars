be17416e8a940b7109c764025654bc9d6b80cc36
Cosme Alves <63716324+cosmealvess@users.noreply.github.com>
2023-02-25 00:47:14 +1100

    commit section 3

--- script.js
+++ script.js
@@ -11,3 +11,14 @@ document.querySelector('.menu').addEventListener('click',() => {
             });
         });
     });
+
+    const videos = document.querySelectorAll('video');
+    videos.forEach((video) => {
+        video.addEventListener('mouseover', () => {
+        video.play();
+
+});
+video.addEventListener('mouseout', () => {
+    video.pause();
+});
+    });
